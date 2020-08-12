ec6f4478cd7831c79c0c32320368c095392f0742
haaryrichardt <53508354+haaryrichardt@users.noreply.github.com>
2020-08-12 18:03:16 +0530

    Update Display.js

--- src/Components/Display.js
+++ src/Components/Display.js
@@ -16,7 +16,7 @@ class Display extends React.Component {
     }
  
     componentDidMount(){
-        axios.get('http://172.105.60.143:3000/Store234')
+        axios.get('http://146.148.89.61:80/Store234')
           .then(res=>{
             const books=res.data;
            
@@ -25,7 +25,7 @@ class Display extends React.Component {
           })
       }
      delbooks(id){
-        axios.delete('http://172.105.60.143:3000/Store234/' +id)
+        axios.delete('http://146.148.89.61:80/Store234/' +id)
         .then(res=>{
             console.log("This id has been deleted"+id);
             alert("The book with id:"+id+"Has been deleted");
